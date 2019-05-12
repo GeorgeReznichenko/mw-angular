2af7d4e5fd5e99358bd08fb2b021311b25feabcd
Andrey Korovin <misticwonder@gmail.com>
2019-05-12 17:30:44 +0300

    config model export

--- libs/mw-angular/complex-filter/src/public-api.ts
+++ libs/mw-angular/complex-filter/src/public-api.ts
@@ -1,3 +1,4 @@
 export * from './lib/complex-filter.module';
 export * from './lib/entities/complex-filter-config.model';
+export * from './lib/entities/complex-filter-component-config.model';
 export * from './lib/modules/material/public-api';
