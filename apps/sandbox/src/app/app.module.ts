e1b331fbbb34c7a58ba4fdcd8f386dbc351c899b
Andrey Korovin <misticwonder@gmail.com>
2019-05-12 15:02:41 +0300

    material installed

--- apps/sandbox/src/app/app.module.ts
+++ apps/sandbox/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComplexFilterModule } from '@mw-angular/complex-filter';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     ComplexFilterModule,
   ],
   declarations: [
