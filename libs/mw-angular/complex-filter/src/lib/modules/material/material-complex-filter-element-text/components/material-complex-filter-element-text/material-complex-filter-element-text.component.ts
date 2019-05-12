a275406b11705bbfb7dc7e73b56ec85eb57439db
Andrey Korovin <misticwonder@gmail.com>
2019-05-12 14:19:22 +0300

    material element text

---
+++ libs/mw-angular/complex-filter/src/lib/modules/material/material-complex-filter-element-text/components/material-complex-filter-element-text/material-complex-filter-element-text.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-material-complex-filter-element-text',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './material-complex-filter-element-text.component.html',
+  styleUrls: ['./material-complex-filter-element-text.component.scss'],
+})
+export class MaterialComplexFilterElementTextComponent {
+}
---
+++ libs/mw-angular/complex-filter/src/lib/modules/material/material-complex-filter-element-text/material-complex-filter-element-text.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {
+  MaterialComplexFilterElementTextComponent
+} from './components/material-complex-filter-element-text/material-complex-filter-element-text.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+    MaterialComplexFilterElementTextComponent,
+  ],
+  entryComponents: [
+    MaterialComplexFilterElementTextComponent,
+  ],
+})
+export class MaterialComplexFilterElementTextModule {
+}
