2cce730085eab6fb642de4e77582faaa46b61ce0
Andrey Korovin <misticwonder@gmail.com>
2019-05-12 18:33:27 +0300

    fix token

--- libs/mw-angular/complex-filter/src/lib/entities/complex-filter-component-data.token.ts
+++ libs/mw-angular/complex-filter/src/lib/entities/complex-filter-component-data.token.ts
@@ -1,3 +1,6 @@
 import { InjectionToken } from '@angular/core';
+import { ComplexFilterComponentDataModel } from './complex-filter-component-data.model';
 
-export const COMPLEX_FILTER_COMPONENT_DATA = new InjectionToken<string>('COMPLEX_FILTER_COMPONENT_DATA');
+export const COMPLEX_FILTER_COMPONENT_DATA = new InjectionToken<ComplexFilterComponentDataModel>(
+  'COMPLEX_FILTER_COMPONENT_DATA',
+);
