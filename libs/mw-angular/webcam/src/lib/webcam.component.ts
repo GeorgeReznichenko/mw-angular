b21201cd3cf45adf1e12f86658bd3a002020729b
Andrey Korovin <misticwonder@gmail.com>
2019-05-13 19:11:53 +0300

    created lib webcam

---
+++ libs/mw-angular/webcam/src/lib/webcam.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mw-webcam',
+  template: `
+    <p>
+      webcam works!
+    </p>
+  `,
+  styles: []
+})
+export class WebcamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ libs/mw-angular/webcam/src/lib/webcam.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { WebcamComponent } from './webcam.component';
+
+@NgModule({
+  declarations: [WebcamComponent],
+  imports: [
+  ],
+  exports: [WebcamComponent]
+})
+export class WebcamModule { }
