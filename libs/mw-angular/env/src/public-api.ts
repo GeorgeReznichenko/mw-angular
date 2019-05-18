7600c13a9df8c4d1e9bef65b415201302f34ab88
Andrey Korovin <misticwonder@gmail.com>
2019-05-18 22:22:39 +0300

    platform service

--- libs/mw-angular/env/src/public-api.ts
+++ libs/mw-angular/env/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of env
- */
-
-export * from './lib/env.service';
-export * from './lib/env.component';
-export * from './lib/env.module';
+export * from './lib/services/platform.service';
