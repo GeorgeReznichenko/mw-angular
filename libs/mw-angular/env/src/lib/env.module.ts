ee8e059c90f0a2d861fac310671cb0c3d2e45bf8
Andrey Korovin <misticwonder@gmail.com>
2019-05-18 22:07:38 +0300

    env lib created

---
+++ libs/mw-angular/env/src/lib/env.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { EnvComponent } from './env.component';
+
+@NgModule({
+  declarations: [EnvComponent],
+  imports: [
+  ],
+  exports: [EnvComponent]
+})
+export class EnvModule { }
