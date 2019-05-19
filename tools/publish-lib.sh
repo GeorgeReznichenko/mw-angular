64d98c0d20aa773706f8eee2eb35d351d08d6630
Andrey Korovin <misticwonder@gmail.com>
2019-05-19 14:37:07 +0300

    upd publish script

--- tools/publish-lib.sh
+++ tools/publish-lib.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 lib="$1"
+basePath="$(pwd)"
 
 if [[ "$lib" != "" ]] ; then
+    cd libs/"$lib"
     npm version patch
     git push origin HEAD
+
+    cd "$basePath"
     ./tools/build-lib.sh "$lib"
+
     cd dist/"$lib"
     npm publish --access public
 else
