ba955833bfa56fdab739f98117209c598cf6ebe2
Andrey Korovin <misticwonder@gmail.com>
2019-06-01 13:07:00 +0300

    breakpoints

--- apps/site/src/app/app-container.component.ts
+++ apps/site/src/app/app-container.component.ts
@@ -6,6 +6,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   template: `
     <h1>Welcome to site!</h1>
     <router-outlet></router-outlet>
+
+    <div class="container">
+      <div class="row">
+        <div class="col col-sm-12" style="background-color: aquamarine">Col 1</div>
+        <div class="col col-sm-12" style="background-color: blueviolet">Col 2</div>
+      </div>
+    </div>
   `,
 })
 export class AppContainerComponent {}
