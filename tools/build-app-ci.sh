52ce41756321376fe4aa78ba73a2edef8b57ae3b
Andrey Korovin <misticwonder@gmail.com>
2019-06-01 13:12:21 +0300

    check build

--- tools/build-app-ci.sh
+++ tools/build-app-ci.sh
@@ -9,6 +9,6 @@ if [[ "$app" != "" && "$env" != "" ]] ; then
     webpack --config ./apps/"$app"/webpack.server.config.js --progress --colors
     node dist/"$app"/prerender.js
 else
-    echo "param errors"
+    echo "ci param errors $app $env"
     exit 1
 fi
