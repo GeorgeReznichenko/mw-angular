1145389bf93571f5aac4c704ae10e1fcdef48fb9
Andrey Korovin <misticwonder@gmail.com>
2019-06-01 15:16:18 +0300

    check fix docker build 2

--- tools/build-app-ci.sh
+++ tools/build-app-ci.sh
@@ -6,7 +6,7 @@ env="$2"
 if [[ "$app" != "" && "$env" != "" ]] ; then
     npx ng build "$app" --delete-output-path --configuration "$env"
     npx ng run "$app":server --delete-output-path
-    webpack --config ./apps/"$app"/webpack.server.config.js --progress --colors
+    npx webpack --config ./apps/"$app"/webpack.server.config.js --progress --colors
     node dist/"$app"/prerender.js
 else
     echo "build app ci param errors"
