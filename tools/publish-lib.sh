fab800204d83d4a291ca6735b4da2cce24ac6c8d
Andrey Korovin <misticwonder@gmail.com>
2019-06-02 13:18:25 +0300

    Publish new version of mw-angular/core

--- tools/publish-lib.sh
+++ tools/publish-lib.sh
@@ -6,8 +6,6 @@ basePath="$(pwd)"
 branch="$(git rev-parse --abbrev-ref HEAD)"
 
 if [[ "$lib" != "" && "$type" != "" && "$branch" == "production" ]] ; then
-   read -p "Enter password from your authenticator: " otp
-
     ./tools/check-lib.sh "$lib"
 
     cd libs/"$lib"
@@ -19,7 +17,10 @@ if [[ "$lib" != "" && "$type" != "" && "$branch" == "production" ]] ; then
 
     ./tools/build-lib.sh "$lib"
 
-    npm publish dist/"$lib" --access public --otp "$otp"
+    echo -n "Enter password from your authenticator: "
+    read otp
+
+    # npm publish dist/"$lib" --access public --otp "$otp"
 else
     echo "param errors or git branch is not production"
     exit 1
