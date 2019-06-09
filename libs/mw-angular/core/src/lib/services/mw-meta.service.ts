30423e5968bd16a5eb37abd434fbc48e8cc4a052
Andrey Korovin <misticwonder@gmail.com>
2019-06-09 20:57:34 +0300

    revert meta service

--- libs/mw-angular/core/src/lib/services/mw-meta.service.ts
+++ libs/mw-angular/core/src/lib/services/mw-meta.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { MwEnvironmentService } from './mw-environment.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MwMetaService {
+  private pageTitleSubject = new BehaviorSubject<string>('');
+
   constructor(private mwEnvironmentService: MwEnvironmentService, private title: Title) {}
 
   setTitle(title: string): any {
@@ -13,5 +16,10 @@ export class MwMetaService {
     const postfix = this.mwEnvironmentService.getValue('titlePostfix', '');
 
     this.title.setTitle(`${prefix}${title}${postfix}`);
+    this.pageTitleSubject.next(title);
+  }
+
+  getPageTitle(): Observable<string> {
+    return this.pageTitleSubject.asObservable();
   }
 }
