69032678fc32e5a58258414bb67327255dabab47
Andrey Korovin <misticwonder@gmail.com>
2019-06-28 18:49:04 +0300

    mat menu

--- apps/site/src/app/modules/material/material.module.ts
+++ apps/site/src/app/modules/material/material.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  exports: [MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule, MatTooltipModule, MatListModule],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatListModule,
+    MatMenuModule,
+  ],
 })
 export class MaterialModule {}
