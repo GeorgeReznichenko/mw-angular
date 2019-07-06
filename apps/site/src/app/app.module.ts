9131847f6bfdec3a45e499b92494d1bfadd9453f
Andrey Korovin <misticwonder@gmail.com>
2019-07-06 16:22:10 +0300

    cache api ssr

--- apps/site/src/app/app.module.ts
+++ apps/site/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MwCoreModule } from '@mw-angular/core';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 import { environment } from '../environments/environment';
 import { AppContainerComponent } from './app-container.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,7 @@ import { TranslationsModule } from './modules/translations/translations.module';
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    TransferHttpCacheModule,
     BrowserAnimationsModule,
     HttpClientModule,
     TranslationsModule,
--- apps/site/src/app/app.server.module.ts
+++ apps/site/src/app/app.server.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { AppContainerComponent } from './app-container.component';
 import { AppModule } from './app.module';
 
 @NgModule({
-  imports: [AppModule, ServerModule, ModuleMapLoaderModule],
+  imports: [AppModule, ServerModule, ServerTransferStateModule, ModuleMapLoaderModule],
   bootstrap: [AppContainerComponent],
 })
 export class AppServerModule {}
