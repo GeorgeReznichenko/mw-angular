e75cd67b888612f1f90ccfd5cbf680d2f4b7fee7
Andrey Korovin <misticwonder@gmail.com>
2019-07-06 14:58:53 +0300

    set langs

--- apps/site/src/app/app-container.component.ts
+++ apps/site/src/app/app-container.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Language } from '../cross-platform/languages/entities/language';
+import { DefaultLanguageService } from '../cross-platform/languages/services/default-language.service';
+import { CurrentLanguageService } from './modules/translations/services/current-language.service';
 
 @Component({
   selector: 'app-container',
@@ -9,4 +13,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     </app-base-layout-container>
   `,
 })
-export class AppContainerComponent {}
+export class AppContainerComponent {
+  private defaultLanguageService: DefaultLanguageService;
+
+  constructor(private currentLanguageService: CurrentLanguageService, private translateService: TranslateService) {
+    this.defaultLanguageService = new DefaultLanguageService();
+
+    this.initTranslations();
+  }
+
+  private initTranslations(): void {
+    this.translateService.setDefaultLang(this.defaultLanguageService.getDefaultLangId());
+
+    this.currentLanguageService.getCurrentLang().subscribe((currentLang: Language) => {
+      this.translateService.use(currentLang.id);
+    });
+  }
+}
